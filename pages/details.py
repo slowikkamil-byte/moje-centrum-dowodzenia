3596ddd620e15f67037b792700772941cce5f288
slowikkamil-byte <slowikkamil92@gmail.com>
2026-01-28 12:42:03 +0100

    Update details.py

--- pages/details.py
+++ pages/details.py
@@ -51,7 +51,7 @@ st.subheader("📸 Zdjęcia i Nagrania")
 st.write("Aby dodać zdjęcia lub nagrania głosowe, kliknij poniższy przycisk. Przeniesie Cię on do bezpiecznego formularza Google, który nie blokuje przesyłu plików.")
 
 # PODMIEŃ TEN LINK NA SWÓJ LINK DO FORMULARZA
-form_url = "https://docs.google.com/forms/d/1vnKSvjbBxdPCnJOvijQ94IbGFt4uyXObB8wHsf1Minc/edit"
+form_url = "https://docs.google.com/forms/d/e/1FAIpQLSfwVDwAYR4sfDJoVs0oW5vL3M03M28H_x_ap9ZL9IvH-k_Z-Q/viewform?usp=publish-editor"
 
 st.link_button("🚀 OTWÓRZ APARAT / DODAJ PLIKI", form_url, use_container_width=True)
 
